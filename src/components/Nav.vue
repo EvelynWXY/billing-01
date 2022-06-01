2341a24961d4cda5f757e6e67b4eeaee584b8cfd
Evelyn <ningyuewushang@163.com>
2022-06-01 15:07:07 +0800

    Money 页面布局

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <div>
+    <div class="nav">
         <router-link to="/money">记账</router-link>
         |
         <router-link to="/labels">标签</router-link>
@@ -11,11 +11,13 @@
 
 <script lang="ts">
 export default {
-
+    name: 'Nav'
 }
 </script>
 
 
-
 <style lang="scss" scoped>
+.nav {
+    border: 1px solid red
+}
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,110 @@
 import Money from '@/views/Money.vue';
 <template>
-    <div>
-        Money.vue
+    <div class="nav-wrapper">
+        <div class="content">Money.vue
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+            <p>Money.vue</p>
+        </div>
+
         <Nav />
     </div>
 </template>
@@ -13,4 +116,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.nav-wrapper {
+    border: 1px solid green;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+}
+
+.content {
+    border: 1px solid blue;
+    overflow: auto;
+    flex-grow: 1; //尽量把所有的高度都给 content
+
+}
 </style>
