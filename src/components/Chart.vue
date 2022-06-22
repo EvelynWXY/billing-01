012bca81ea92e842d356d4398de8f11be1c881fa
EvelynWXY <ningyuewushang@163.com>
2022-06-22 18:06:57 +0800

    完善 chart 的细节样式

---
+++ src/components/Chart.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="wrapper" ref="wrapper">
+    </div>
+</template>
+
+<script lang="ts">
+
+import { Component, Prop, Vue } from 'vue-property-decorator';
+// import echarts, { EChartsOption, ECharts } from 'echarts';
+import * as echarts from 'echarts/core';
+import { ECharts, EChartsOption } from 'echarts';
+
+@Component
+export default class Chart extends Vue {
+    @Prop() options?: EChartsOption;
+    chart?: ECharts;
+    mounted() {
+        if (this.options === undefined) {
+            return console.log('options 为空');
+        }
+        const chart = echarts.init(this.$refs.wrapper as HTMLDivElement);
+        chart.setOption(this.options!)
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+    height: 400px;
+}
+</style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,7 +2,10 @@
 
     <Layout>
         <Tabs classPrefix="type" :dataSource="recordTypeList" :value.sync="type" />
+        <div class="chart-wrapper" ref="chartWrapper">
+            <Chart class="chart" :options="x" />
 
+        </div>
         <!-- 用JS配置 height
             <Tabs classPrefix="interval" :dataSource="intervalList" :value.sync="interval" height="48px" /> -->
 
@@ -36,14 +39,19 @@ import Tabs from '@/components/Tabs.vue';
 import recordTypeList from '@/constants/recordTypeList';
 import dayjs from 'dayjs';
 import clone from '@/lib/clone';
+import Chart from '@/components/Chart.vue';
+
 
 
 @Component({
-    components: { Tabs }
+    components: { Tabs, Chart }
 })
 export default class Statistics extends Vue {
     tagString(tags: Tag[]) {
         return tags.length === 0 ? '无' : tags.map(t => t.name).join(',');
+    }
+    mounted() {
+        (this.$refs.chartWrapper as HTMLDivElement).scrollLeft = 9999;
     }
     beautify(string: string) {
         const now = dayjs();
@@ -60,6 +68,53 @@ export default class Statistics extends Vue {
             return day.format('YYYY年M月D日');
         }
 
+    }
+    get x() {
+        return {
+            grid: {
+                left: 0,
+                right: 0
+            },
+            xAxis: {
+                type: 'category',
+                data: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+                    '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+                    '21', '22', '23', '24', '25', '26', '27', '28', '29', '30'
+                ],
+                axisTick: {
+                    alignWithLabel: true
+                },
+                axisLine: {
+                    lineStyle: {
+                        color: '#666'
+                    }
+                }
+            },
+            yAxis: {
+                type: 'value',
+                show: false,
+            },
+            series: [{
+                data: [820, 932, 901, 934, 1290, 1330, 1320,
+                    820, 932, 901, 934, 1290, 1330, 1320,
+                    820, 932, 901, 934, 1290, 1330, 1320,
+                    820, 932, 901, 934, 1290, 1330, 1320, 1, 2
+                ],
+                type: 'line',
+                symbol: 'circle',
+                symbolSize: 12,
+                itemStyle: {
+                    color: "#9295f3",
+                    borderWidth: 1,
+                    borderColor: "#9295f3"
+                }
+            }],
+            tooltip: {
+                show: true, triggerOn: 'click',
+                position: 'top',
+                formatter: '{c}'
+            }
+        };
     }
     get recordList() {
         return (this.$store.state as RootState).recordList;
@@ -164,5 +219,17 @@ export default class Statistics extends Vue {
     margin-right: auto;
     margin-left: 16px;
     color: #999;
+}
+
+.chart {
+    width: 430%;
+
+    &-wrapper {
+        overflow: auto;
+
+        &::-webkit-scrollbar {
+            display: none;
+        }
+    }
 }
 </style>
